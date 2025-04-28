847b6a2192f4383545cca8968071bac67006bcb0
Tarun Murali <tarunmurali02@gmail.com>
2025-04-28 02:16:41 -0400

    4

--- chatapp-back/src/main/java/com/example/chatapp_back/repository_stuff/JpaUserRepository.java
+++ chatapp-back/src/main/java/com/example/chatapp_back/repository_stuff/JpaUserRepository.java
@@ -4,12 +4,10 @@ package com.example.chatapp_back.repository_stuff;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.example.chatapp_back.infra.UserEntity;
 
-import com.example.chatapp_back.messaging_stuff.User;
 
-public interface JpaUserRepository extends JpaRepository<User, Long> {
+public interface JpaUserRepository extends JpaRepository<UserEntity, Long> {
 
 }
- {
-    
-}
+ 
